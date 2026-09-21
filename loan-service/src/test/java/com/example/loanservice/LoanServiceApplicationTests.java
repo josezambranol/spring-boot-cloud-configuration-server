256a5ad7bd4caa8a1eb6baa5c0d46dffbeb49a99
Jose Daniel Zambrano Luna <josedaniellunamarimon@gmail.com>
2026-09-20 20:15:00 -0500

    Segundo avance

---
+++ loan-service/src/test/java/com/example/loanservice/LoanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {"application.message=Test Message"})
+class LoanServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
